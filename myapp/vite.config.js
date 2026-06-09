1512a2fc755809e10d72e558eb8bbd31c9e251c4
Dushyanthi-B <176794402+Dushyanthi-B@users.noreply.github.com>
2026-06-09 13:43:15 +0530

    Update Vite config with GitHub Pages base path

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/final-k-store/',
   plugins: [react()],
   server: {
     port: 5173, // Desired port
